a97032198e4e23354b7c28dee5559433b9e6381b
sadchill82 <nurmuhametoffislam@yandex.ru>
2022-12-04 02:35:39 +0300

    add auth screen

---
+++ Image Feed/Constants.swift
@@ -0,0 +1,7 @@
+import Foundation
+
+let AccessKey = "66ifq6ApgtDPRs5i_2UKl5SBYuJD7Uyd3z6oXnQVGiE"
+let SecretKey = "ycMHBGmZwTBPQGTieDqZQflJn0Ot91tMeGE1Q414s0U"
+let RedirectURI = "urn:ietf:wg:oauth:2.0:oob"
+let AccessScope = "public+read_user+write_likes"
+let DefaultBaseURL = URL(string: "https://api.unsplash.com")!
